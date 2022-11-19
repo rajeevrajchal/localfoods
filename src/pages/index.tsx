9c6b5c99726958c7c73edbdcd57acd4538282506
rajeev rajchal <rajeev@nextlevelvalue.nl>
2022-11-19 10:01:52 +0545

    basic setup

---
+++ src/pages/index.tsx
@@ -0,0 +1,13 @@
+import { Box, Typography } from "@mui/material";
+
+export default function Home() {
+  return (
+    <Box
+      sx={{
+        bgcolor: "red",
+      }}
+    >
+      <Typography>Hello world</Typography>
+    </Box>
+  );
+}
